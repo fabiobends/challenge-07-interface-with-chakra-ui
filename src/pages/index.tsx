80fd3a605d6e77d70e73b5e962b652283e87e292
Fábio <fabio.mendes@profusion.com>
2021-09-21 21:12:09 -0300

    feat: swiper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import { Box, Flex, Image, Heading, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export default function Home() {
   return (
@@ -11,17 +17,45 @@ export default function Home() {
         backgroundSize="cover"
         height={400}
         display="flex"
-        justify="space-between"
       >
-        <Box>
+        <Box py="100" ml="100">
           <Heading>5 Continentes, infinitas possibilidades</Heading>
           <Text>
             Chegou a hora de tirar do papel a viagem que você sempre sonhou
           </Text>
         </Box>
-        <Image position="absolute" src="/assets/svg/Airplane.svg" alt="Avião" />
+        <Image
+          // position="absolute"
+          width="500px"
+          right="200"
+          bottom="-50"
+          src="/assets/svg/Airplane.svg"
+          alt="Avião"
+        />
       </Box>
-      {/* <Image src="/assets/banners/Background.png" width="100%" /> */}
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <Image
+            src="/assets/banners/banner_europe.png"
+            alt="banner"
+            fit="contain"
+            width="100%"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src="/assets/banners/banner_europe.png"
+            alt="banner"
+            fit="contain"
+            width="100%"
+          />
+        </SwiperSlide>
+      </Swiper>
     </Box>
   );
 }
